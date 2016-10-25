b2e4dab220f3a90d5f4aa55d1cb18b7084e965a5
Jian Bai (Eric Pai) <ericpai94@hotmail.com>
2016-10-25 09:58:09 +0800

    Fix wrong dependencies

--- tail.go
+++ tail.go
@@ -15,9 +15,9 @@ import (
 	"sync"
 	"time"
 
-	"github.com/hpcloud/tail/ratelimiter"
-	"github.com/hpcloud/tail/util"
-	"github.com/hpcloud/tail/watch"
+	"github.com/ericpai/tail/ratelimiter"
+	"github.com/ericpai/tail/util"
+	"github.com/ericpai/tail/watch"
 	"gopkg.in/tomb.v1"
 )
 
--- tail_test.go
+++ tail_test.go
@@ -14,8 +14,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/hpcloud/tail/ratelimiter"
-	"github.com/hpcloud/tail/watch"
+	"github.com/ericpai/tail/ratelimiter"
+	"github.com/ericpai/tail/watch"
 )
 
 func init() {
